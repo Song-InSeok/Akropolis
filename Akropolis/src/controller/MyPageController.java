e0643b1c70706ac2eecf6225dad76e9757d06770
younghak <nicemuy@gmail.com>
2012-11-17 02:45:06 +0900

    Controller에 대한 설명 주석달음

--- Akropolis/src/controller/MyPageController.java
+++ Akropolis/src/controller/MyPageController.java
@@ -11,13 +11,34 @@ import bean.BeanTest;
 
 @RootURL("/mypage")
 public class MyPageController {
-	@Mapping(url="/ssss.ap",bean="bean.BeanTest")
-	ModelView deco(HttpServletRequest request,HttpServletResponse response,Object bean){
+	@Mapping(url="/decotest.ap",bean="bean.BeanTest") //bean 사용 안할시 bean 빼면됨
+	ModelView deco(HttpServletRequest request,HttpServletResponse response,Object bean){ // bean 사용 안할시 Object bean 빼면됨
 		//Model(Bean)
-		BeanTest bt = (BeanTest)bean;
-		System.out.println(bt.getId());
-		System.out.println(bt.getPwd());
+		BeanTest bt = (BeanTest)bean; //캐스팅해서 적절히 사용
 		ModelView mv = new ModelView("/mypage/decotest");
+		
+		//request.setAttribute("model",mv); 가 자동으로 등록됨
+		//따라서 꺼낼시에  ((ModelView)request.getAttribute("model")).getModel("id"); 로 꺼낸다
+		mv.setModel("id", "younghak");
 		return mv;
 	}
+	
+	/*
+	 * Controller 클래스 기본 형태1
+	 * @RootURL(Contextpath 제외한 URL에서 앞부분에서 매칭될 부분)
+	 * 
+	 * 함수의 기본 형태 1
+	 * @Mapping(url=RootURL을 제외한 나머지 URL에서 매칭될 부분[,bean=bean의 풀패스 클래스 이름])
+	 * ModelView 함수이름(HttpServletRequest request,HttpServletResponse response){
+	 * 		ModelView mv = new ModelView(뷰이름);
+	 * 		return mv;
+	 * }
+	 * 
+	 * 함수의 기본 형태 2(bean을 사용 하는 경우)
+	 * @Mapping(url=RootURL을 제외한 나머지 URL에서 매칭될 부분[,bean=bean의 풀패스 클래스 이름])
+	 * ModelView 함수이름(HttpServletRequest request,HttpServletResponse response,Object bean){
+	 * 		ModelView mv = new ModelView(뷰이름);
+	 * 		return mv;
+	 * }
+	 */
 }
