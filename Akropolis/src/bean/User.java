42e7e38eef4bebaff207fdb0d23c020478bdc3cc
LeeYoungHak <nicemuy@gmail.com>
2012-11-23 02:29:43 +0900

    src 빠트린거..

---
+++ Akropolis/src/bean/User.java
@@ -0,0 +1,47 @@
+package bean;
+
+public class User {
+	private String email;
+	private String name;
+	private String say;
+	private String photo;
+	private int honor;
+	private int dishonor;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSay() {
+		return say;
+	}
+	public void setSay(String say) {
+		this.say = say;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	public int getHonor() {
+		return honor;
+	}
+	public void setHonor(int honor) {
+		this.honor = honor;
+	}
+	public int getDishonor() {
+		return dishonor;
+	}
+	public void setDishonor(int dishonor) {
+		this.dishonor = dishonor;
+	}
+	
+}
---
+++ Akropolis/src/dao/UserDAO.java
@@ -0,0 +1,27 @@
+package dao;
+
+import mapper.UserMapper;
+import mybatis.config.MyBatisManager;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import bean.User;
+
+public class UserDAO {
+	public static SqlSessionFactory sqlSessionFactory = MyBatisManager.getInstance();
+	
+	public User getUser(String email){
+		SqlSession session = sqlSessionFactory.openSession();
+		User user = null;
+		try{
+			UserMapper mapper = session.getMapper(UserMapper.class);
+			user = mapper.selectUser(email);
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			session.close();
+		}
+		return user;
+	}
+}
