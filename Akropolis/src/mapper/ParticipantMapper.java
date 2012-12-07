e0e5085157acf43ea8cbc3bece161c0d2c6a9fca
pooingx2 <pooingx2@gmail.com>
2012-12-07 21:31:35 +0900

    debate.css

--- Akropolis/src/mapper/ParticipantMapper.java
+++ Akropolis/src/mapper/ParticipantMapper.java
@@ -7,9 +7,5 @@ import bean.Participant;
 public interface ParticipantMapper {
 	Participant selectParticipant(String email);
 	List<Participant> getParticipants(int mt_id);
-<<<<<<< HEAD
 	Participant getParticipant(Participant par);
-
-=======
->>>>>>> 712f7be99456a6e5867844dc7bee32904a8fdad0
 }
