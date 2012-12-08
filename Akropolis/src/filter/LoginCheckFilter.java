2d0062053a38c2f895f884655ec6bb9d5b0c9d7c
LeeYoungHak <nicemuy@gmail.com>
2012-12-09 06:22:35 +0900

    mypage 비로그인 막음

---
+++ Akropolis/src/filter/LoginCheckFilter.java
@@ -0,0 +1,67 @@
+package filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class LoginCheckFilter
+ */
+@WebFilter(filterName="LoginCheck",urlPatterns={"/mypage/*"})
+public class LoginCheckFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public LoginCheckFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		HttpSession session = httpRequest.getSession(false);
+		
+		boolean login = false;
+		if(session != null){
+			if(session.getAttribute("user") != null){
+				login = true;
+			}
+		}
+		// pass the request along the filter chain
+		if(login){
+			chain.doFilter(request, response);
+		}else{
+			httpResponse.sendRedirect("/Akropolis/main.ap");
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
